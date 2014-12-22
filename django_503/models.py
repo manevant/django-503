e7984a029b27dc76c9cf54a2faa7017181933564
Anton <manevant@gmail.com>
2014-12-22 23:57:21 +0300

    BooleanField no longer defaults to False
    When a BooleanField doesn’t have an explicit default, the implicit default value is None. In previous version of Django, it was False, but that didn’t represent accurately the lack of a value.
    https://django.readthedocs.org/en/latest/releases/1.6.html#booleanfield-no-longer-defaults-to-false

--- django_503/models.py
+++ django_503/models.py
@@ -3,4 +3,4 @@ from django.utils.translation import ugettext as _
 
 class Config(models.Model):
     key = models.CharField(_('Key'), max_length=100, unique=True)
-    value = models.BooleanField(_('Value'))
+    value = models.BooleanField(_('Value'), default=None)
